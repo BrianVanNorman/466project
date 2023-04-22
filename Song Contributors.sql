46334141de80e9a5e4c04b0122ccaf5181ee3f09
Corey Wojslaw <89593319+z1865134@users.noreply.github.com>
2023-04-21 21:35:22 -0500

    Add files via upload
    Code to insert into tables

---
+++ Song Contributors.sql
@@ -0,0 +1,381 @@
+--Inserts for songs and artists
+INSERT INTO Song VALUES (1,"As It Was","Harry Styles");
+INSERT INTO Song VALUES (2,"About Damn Time","Lizzo");
+INSERT INTO Song VALUES (3,"Bam Bam","Camila Cabello");
+INSERT INTO Song VALUES (4,"Supermodel","Måneskin");
+INSERT INTO Song VALUES (5,"The Joker and the Queen","Ed Sheeran");
+INSERT INTO Song VALUES (6,"First Class","Jack Harlow");
+INSERT INTO Song VALUES (7,"She's All I Wanna Be","Tate McRae");
+INSERT INTO Song VALUES (8,"Hold My Hand","Lady Gaga");
+INSERT INTO Song VALUES (9,"Fingers Crossed","Lauren Spencer-Smith");
+INSERT INTO Song VALUES (10,"Baby","Aitch");
+INSERT INTO Song VALUES (11,"Black Summer","Red Hot Chili Peppers");
+INSERT INTO Song VALUES (12,"Boyfriend","Dove Cameron");
+INSERT INTO Song VALUES (13,"Green Green Grass","George Ezra");
+INSERT INTO Song VALUES (14,"2Step","Ed Sheeran");
+INSERT INTO Song VALUES (15,"Light Switch","Charlie Puth");
+INSERT INTO Song VALUES (16,"Won't Stand Down","Muse");
+INSERT INTO Song VALUES (17,"Bones","Imagine Dragons");
+INSERT INTO Song VALUES (18,"Late Night Talking","Harry Styles");
+INSERT INTO Song VALUES (19,"Words","Alesso");
+INSERT INTO Song VALUES (20,"House On Fire","Mimi Webb");
+INSERT INTO Song VALUES (21,"Anyone For You","George Ezra");
+INSERT INTO Song VALUES (22,"Numb Little Buf","Em Beihold");
+INSERT INTO Song VALUES (23,"Toast","Koffee");
+INSERT INTO Song VALUES (24,"Sacrifice","The Weekend");
+INSERT INTO Song VALUES (25,"Crazy What Love Can Do","David Guetta");
+INSERT INTO Song VALUES (26,"Attention","Justin Bieber");
+INSERT INTO Song VALUES (27,"Celebrity Skin","Doja Cat");
+INSERT INTO Song VALUES (28,"Space Man","Sam Ryder");
+INSERT INTO Song VALUES (29,"Flowers","Lauren Spencer-Smith");
+INSERT INTO Song VALUES (30,"Baby","Charli XCX");
+--Inserts for Contributors
+--As It Was
+INSERT INTO Contributors VALUES(1,"Kid Harpoon");
+INSERT INTO Contributors VALUES(2,"Doug Showalter");
+INSERT INTO Contributors VALUES(3,"Tyler Johnson");
+INSERT INTO Contributors VALUES(4,"Katie May");
+INSERT INTO Contributors VALUES(5,"Jermy Hatcher");
+--About Damn Time
+INSERT INTO Contributors VALUES(6,"Blake Slatkin");
+INSERT INTO Contributors VALUES(7,"Ricky Reed");
+INSERT INTO Contributors VALUES(8,"Victor Indrizzo");
+INSERT INTO Contributors VALUES(9,"Patrick Kehrier");
+INSERT INTO Contributors VALUES(10,"Eric Frederic");
+INSERT INTO Contributors VALUES(11,"Mike Wright");
+--Bam Bam
+INSERT INTO Contributors VALUES(12,"Ed Sheeran");
+INSERT INTO Contributors VALUES(13,"Scott Harris");
+INSERT INTO Contributors VALUES(14,"Ramon Stagnaro");
+INSERT INTO Contributors VALUES(15,"George Shelby");
+INSERT INTO Contributors VALUES(16,"Bill Malina");
+INSERT INTO Contributors VALUES(17,"Daniel Uribe");
+INSERT INTO Contributors VALUES(18,"Manny Marroquin");
+--Supermodel
+INSERT INTO Contributors VALUES(19,"Max Martin");
+INSERT INTO Contributors VALUES(20,"Thomas Raggi");
+INSERT INTO Contributors VALUES(21,"Ethan Torchio");
+INSERT INTO Contributors VALUES(22,"Jermy Lertola");
+INSERT INTO Contributors VALUES(23,"Rami Yacoub");
+INSERT INTO Contributors VALUES(24,"Spike Stent");
+--The Joker and the Queen
+INSERT INTO Contributors VALUES(25,"Taylor Swift");
+INSERT INTO Contributors VALUES(26,"Fred");
+INSERT INTO Contributors VALUES(27,"Romans");
+INSERT INTO Contributors VALUES(28,"Ashok Klouda");
+INSERT INTO Contributors VALUES(29,"Grahm Archer");
+INSERT INTO Contributors VALUES(30,"Mark Stent");
+--First Class
+INSERT INTO Contributors VALUES(31,"Jasper Harris");
+INSERT INTO Contributors VALUES(32,"Nickie Job Pabon");
+INSERT INTO Contributors VALUES(33,"Angel Lopez");
+INSERT INTO Contributors VALUES(34,"Dale Becker");
+--She's All I Wanna Be (Uses ID 28 Mark Stent)
+INSERT INTO Contributors VALUES(35,"Greg Kurstin");
+INSERT INTO Contributors VALUES(36,"Dave Kutch");
+INSERT INTO Contributors VALUES(37,"Grek Kursin");
+--Hold My Hand
+INSERT INTO Contributors VALUES(38,"Michael Tucker");
+INSERT INTO Contributors VALUES(39,"Tim Stewart");
+INSERT INTO Contributors VALUES(40,"BloodPop");
+INSERT INTO Contributors VALUES(41,"Tom Norris");
+--Fingers Crossed
+INSERT INTO Contributors VALUES(42,"Fransisca Hall");
+INSERT INTO Contributors VALUES(43,"T.I Jakke");
+--Baby (Used ID26 Fred)
+INSERT INTO Contributors VALUES(44,"Ashanti");
+INSERT INTO Contributors VALUES(45,"Benjy Gibson");
+INSERT INTO Contributors VALUES(46,"Marco Parisi");
+INSERT INTO Contributors VALUES(47,"Berwyn");
+INSERT INTO Contributors VALUES(48,"Stuart Hawkes");
+INSERT INTO Contributors VALUES(49,"Jay Reynolds");
+--Black Summer
+INSERT INTO Contributors VALUES(50,"Flea");
+INSERT INTO Contributors VALUES(51,"John Frusciante");
+INSERT INTO Contributors VALUES(52,"Chad Smith");
+INSERT INTO Contributors VALUES(53,"Rick Rubin");
+INSERT INTO Contributors VALUES(54,"Anthony Kiedis");
+--Boyfriend
+INSERT INTO Contributors VALUES(55,"Skyler Stonestreet");
+INSERT INTO Contributors VALUES(56,"Eric Lagg");
+INSERT INTO Contributors VALUES(57,"Jeff Kopchia");
+INSERT INTO Contributors VALUES(58,"Alex Ghenea");
+--Green Green Grass
+INSERT INTO Contributors VALUES(59,"Joel Pott");
+INSERT INTO Contributors VALUES(60,"Stuart Price");
+INSERT INTO Contributors VALUES(61,"Fabio De Oliveira");
+INSERT INTO Contributors VALUES(62,"Daniel Moyler");
+INSERT INTO Contributors VALUES(64,"Majestic");
+--2Step
+INSERT INTO Contributors VALUES(65,"Lil Baby");
+INSERT INTO Contributors VALUES(66,"Louis Bell");
+INSERT INTO Contributors VALUES(67,"Watt");
+INSERT INTO Contributors VALUES(68,"Stuart Hawkes");
+--Light Switch(Uses ID18 Manny Marroquin)
+INSERT INTO Contributors VALUES(69,"Jake Torrey");
+INSERT INTO Contributors VALUES(70,"Jan Ozveren");
+INSERT INTO Contributors VALUES(71,"Scott Desmarais");
+INSERT INTO Contributors VALUES(72,"Charlie Puth");
+--Won't Stand Down
+INSERT INTO Contributors VALUES(73,"Matt Bellamy");
+INSERT INTO Contributors VALUES(74,"Dominic Howard");
+INSERT INTO Contributors VALUES(75,"Chris Wolstenholme");
+INSERT INTO Contributors VALUES(76,"Aleks Von korff");
+INSERT INTO Contributors VALUES(78,"Muse");
+INSERT INTO Contributors VALUES(79,"Ban Lancaster");
+--Bones
+INSERT INTO Contributors VALUES(80,"Dan Reynolds");
+INSERT INTO Contributors VALUES(81,"Daniel Wayne Sermon");
+INSERT INTO Contributors VALUES(82,"Daniel Platzman");
+INSERT INTO Contributors VALUES(83,"Andrew Tolman");
+INSERT INTO Contributors VALUES(84,"Randy Merrill");
+INSERT INTO Contributors VALUES(85,"Mattman & Robin");
+--Late Night Talking(Using ID1 Kid Harpoon, ID84 Randy Merrill, ID3 Tyler Johnson)
+INSERT INTO Contributors VALUES(86,"Mike Stent");
+--Words(Using ID84 Randy Merrill)
+INSERT INTO Contributors VALUES(87,"Zara Larsson");
+INSERT INTO Contributors VALUES(88,"Karen Ann Poole");
+INSERT INTO Contributors VALUES(89,"Alesso");
+--House On Fire
+INSERT INTO Contributors VALUES(90,"Bowman");
+INSERT INTO Contributors VALUES(91,"Dave Kutch");
+INSERT INTO Contributors VALUES(92,"Cirkut");
+INSERT INTO Contributors VALUES(93,"the Nocturns");
+--Anyone For You(Using ID59 Joel Pott, ID61 Fabio De Oliveira)
+INSERT INTO Contributors VALUES(94,"Matt Colton");
+INSERT INTO Contributors VALUES(95,"Dan Grech-Marguerat");
+--Numb Little Bug(Using ID84 Randy Merrill)
+INSERT INTO Contributors VALUES(96,"Nick Lopez");
+INSERT INTO Contributors VALUES(97,"Dru DeCaro");
+INSERT INTO Contributors VALUES(98,"Mitch McCarthy");
+--Toast
+INSERT INTO Contributors VALUES(99,"IzyBeats");
+INSERT INTO Contributors VALUES(100,"Walshy Fire");
+--Sacrifice
+INSERT INTO Contributors VALUES(101,"Steve Angello");
+INSERT INTO Contributors VALUES(102,"Shin Kamiyama");
+INSERT INTO Contributors VALUES(103,"Oscar Holter");
+INSERT INTO Contributors VALUES(104,"Serban Ghenea");
+--Crazy What Love Can Do
+INSERT INTO Contributors VALUES(105,"Ella Henderson");
+INSERT INTO Contributors VALUES(106,"Becky Hill");
+INSERT INTO Contributors VALUES(107,"Jordan Riley");
+INSERT INTO Contributors VALUES(108,"David Guetta");
+INSERT INTO Contributors VALUES(109,"New Levels");
+INSERT INTO Contributors VALUES(110,"Peppe Folliero");
+--Attention
+INSERT INTO Contributors VALUES(111,"Omah Lay");
+INSERT INTO Contributors VALUES(112,"Felisha King");
+INSERT INTO Contributors VALUES(113,"Avedon");
+INSERT INTO Contributors VALUES(114,"Mark Parfitt");
+INSERT INTO Contributors VALUES(115,"HARV");
+INSERT INTO Contributors VALUES(116,"Josh Gudwin");
+--Celebrity Skin
+INSERT INTO Contributors VALUES(117,"Billy Corgan");
+INSERT INTO Contributors VALUES(118,"Travis Barker");
+INSERT INTO Contributors VALUES(119,"Bryce Bordone");
+INSERT INTO Contributors VALUES(120,"Dark Waves");
+INSERT INTO Contributors VALUES(121,"Yeti Beats");
+--Space Man(Using ID95 Dan Grech-Marguerat)
+INSERT INTO Contributors VALUES(122,"Amy Wadge");
+INSERT INTO Contributors VALUES(123,"KOZ");
+INSERT INTO Contributors VALUES(124,"Matt Snell");
+--Flowers
+INSERT INTO Contributors VALUES(125,"Cian Ducrot");
+INSERT INTO Contributors VALUES(126,"Joe LaPorta");
+INSERT INTO Contributors VALUES(127,"Andrew Maury");
+--Baby
+INSERT INTO Contributors VALUES(128,"Justin Raisen");
+INSERT INTO Contributors VALUES(129,"SADPONY");
+INSERT INTO Contributors VALUES(130,"Anthony Paut Lopez");
+INSERT INTO Contributors VALUES(131,"Geoff Swan");
+--Insert for Contributions
+--As It Was
+INSERT INTO Contribution VALUES(1,1,"Songwriter");
+INSERT INTO Contribution VALUES(2,1,"Guitar");
+INSERT INTO Contribution VALUES(3,1,"Percussion/Extra");
+INSERT INTO Contribution VALUES(4,1,"Engineer");
+INSERT INTO Contribution VALUES(3,1,"Producers");
+INSERT INTO Contribution VALUES(5,1,"Recording");
+--About Damn Time
+INSERT INTO Contribution VALUES(6,2,"Songwriter");
+INSERT INTO Contribution VALUES(7,2,"Guitar");
+INSERT INTO Contribution VALUES(8,2,"Percussion/Extra");
+INSERT INTO Contribution VALUES(9,2,"Engineer");
+INSERT INTO Contribution VALUES(10,2,"Producers");
+INSERT INTO Contribution VALUES(11,2,"Recording");
+--Bam Bam
+INSERT INTO Contribution VALUES(12,3,"Feature");
+INSERT INTO Contribution VALUES(13,3,"Songwriter");
+INSERT INTO Contribution VALUES(14,3,"Guitar");
+INSERT INTO Contribution VALUES(15,3,"Percussion/Extra");
+INSERT INTO Contribution VALUES(16,3,"Engineer");
+INSERT INTO Contribution VALUES(17,3,"Producers");
+INSERT INTO Contribution VALUES(18,3,"Recording");
+--Supermodel
+INSERT INTO Contribution VALUES(19,4,"Songwriter");
+INSERT INTO Contribution VALUES(20,4,"Guitar");
+INSERT INTO Contribution VALUES(21,4,"Percussion/Extra");
+INSERT INTO Contribution VALUES(22,4,"Engineer");
+INSERT INTO Contribution VALUES(23,4,"Producers");
+INSERT INTO Contribution VALUES(24,4,"Recording");
+--The Joker and the Queen
+INSERT INTO Contribution VALUES(25,5,"Feature");
+INSERT INTO Contribution VALUES(26,5,"Songwriter");
+INSERT INTO Contribution VALUES(27,5,"Guitar");
+INSERT INTO Contribution VALUES(28,5,"Percussion/Extra");
+INSERT INTO Contribution VALUES(29,5,"Engineer");
+INSERT INTO Contribution VALUES(30,5,"Recording");
+--First Class
+INSERT INTO Contribution VALUES(31,6,"Songwriter");
+INSERT INTO Contribution VALUES(32,6,"Engineer");
+INSERT INTO Contribution VALUES(33,6,"Producers");
+INSERT INTO Contribution VALUES(34,6,"Recording");
+--She's All I Wanna Be
+INSERT INTO Contribution VALUES(35,7,"Songwriter");
+INSERT INTO Contribution VALUES(36,7,"Engineer");
+INSERT INTO Contribution VALUES(37,7,"Producers");
+INSERT INTO Contribution VALUES(28,7,"Recording");
+--Hold My Hand
+INSERT INTO Contribution VALUES(38,8,"Songwriter");
+INSERT INTO Contribution VALUES(39,8,"Guitar");
+INSERT INTO Contribution VALUES(40,8,"Producers");
+INSERT INTO Contribution VALUES(41,8,"Recording");
+--Fingers Crossed
+INSERT INTO Contribution VALUES(42,9,"Songwriter");
+INSERT INTO Contribution VALUES(43,9,"Engineer");
+INSERT INTO Contribution VALUES(43,9,"Producers");
+INSERT INTO Contribution VALUES(43,9,"Recording");
+--Baby
+INSERT INTO Contribution VALUES(44,10,"Feature");
+INSERT INTO Contribution VALUES(45,10,"Songwriter");
+INSERT INTO Contribution VALUES(46,10,"Guitar");
+INSERT INTO Contribution VALUES(47,10,"Percussion/Extra");
+INSERT INTO Contribution VALUES(48,10,"Engineer");
+INSERT INTO Contribution VALUES(26,10,"Producers");
+INSERT INTO Contribution VALUES(49,10,"Recording");
+--Black Summer
+INSERT INTO Contribution VALUES(50,11,"Songwriter");
+INSERT INTO Contribution VALUES(51,11,"Guitar");
+INSERT INTO Contribution VALUES(52,11,"Percussion/Extra");
+INSERT INTO Contribution VALUES(50,11,"Bass");
+INSERT INTO Contribution VALUES(53,11,"Producers");
+INSERT INTO Contribution VALUES(54,11,"Vocals");
+--Boyfriend
+INSERT INTO Contribution VALUES(55,12,"Songwriter");
+INSERT INTO Contribution VALUES(56,12,"Engineer");
+INSERT INTO Contribution VALUES(57,12,"Producers");
+INSERT INTO Contribution VALUES(58,12,"Recording");
+--Green Green Grass
+INSERT INTO Contribution VALUES(59,13,"Songwriter");
+INSERT INTO Contribution VALUES(60,13,"Guitar");
+INSERT INTO Contribution VALUES(61,13,"Percussion/Extra");
+INSERT INTO Contribution VALUES(62,13,"Engineer");
+INSERT INTO Contribution VALUES(61,13,"Producers");
+INSERT INTO Contribution VALUES(64,13,"Recording");
+--2Step
+INSERT INTO Contribution VALUES(65,14,"Feature");
+INSERT INTO Contribution VALUES(66,14,"Songwriter");
+INSERT INTO Contribution VALUES(67,14,"Guitar");
+INSERT INTO Contribution VALUES(68,14,"Engineer");
+INSERT INTO Contribution VALUES(67,14,"Producers");
+--Light Switch
+INSERT INTO Contribution VALUES(69,15,"Songwriter");
+INSERT INTO Contribution VALUES(70,15,"Guitar");
+INSERT INTO Contribution VALUES(71,15,"Engineer");
+INSERT INTO Contribution VALUES(72,15,"Producers");
+INSERT INTO Contribution VALUES(18,15,"Recording");
+--Won't Stand Down
+INSERT INTO Contribution VALUES(73,16,"Songwriter");
+INSERT INTO Contribution VALUES(73,16,"Guitar");
+INSERT INTO Contribution VALUES(74,16,"Percussion/Extra");
+INSERT INTO Contribution VALUES(75,16,"Bass");
+INSERT INTO Contribution VALUES(76,16,"Engineer");
+INSERT INTO Contribution VALUES(78,16,"Producers");
+INSERT INTO Contribution VALUES(79,16,"Recording");
+--Bones
+INSERT INTO Contribution VALUES(80,17,"Songwriter");
+INSERT INTO Contribution VALUES(81,17,"Guitar");
+INSERT INTO Contribution VALUES(82,17,"Percussion/Extra");
+INSERT INTO Contribution VALUES(83,17,"Engineer");
+INSERT INTO Contribution VALUES(84,17,"Producers");
+INSERT INTO Contribution VALUES(85,17,"Recording");
+--Late Night Talking
+INSERT INTO Contribution VALUES(1,18,"Songwriter");
+INSERT INTO Contribution VALUES(1,18,"Guitar");
+INSERT INTO Contribution VALUES(1,18,"Percussion/Extra");
+INSERT INTO Contribution VALUES(84,18,"Engineer");
+INSERT INTO Contribution VALUES(3,18,"Producers");
+INSERT INTO Contribution VALUES(86,18,"Recording");
+--Words
+INSERT INTO Contribution VALUES(87,19,"Feature");
+INSERT INTO Contribution VALUES(88,19,"Songwriter");
+INSERT INTO Contribution VALUES(84,19,"Engineer");
+INSERT INTO Contribution VALUES(89,19,"Producers");
+INSERT INTO Contribution VALUES(89,19,"Recording");
+--House On Fire
+INSERT INTO Contribution VALUES(90,20,"Songwriter");
+INSERT INTO Contribution VALUES(91,20,"Engineer");
+INSERT INTO Contribution VALUES(92,20,"Producers");
+INSERT INTO Contribution VALUES(93,20,"Recording");
+--Anyone For You
+INSERT INTO Contribution VALUES(59,21,"Songwriter");
+INSERT INTO Contribution VALUES(59,21,"Guitar");
+INSERT INTO Contribution VALUES(61,21,"Percussion/Extra");
+INSERT INTO Contribution VALUES(94,21,"Engineer");
+INSERT INTO Contribution VALUES(59,21,"Producers");
+INSERT INTO Contribution VALUES(95,21,"Recording");
+--Numb Little Bug
+INSERT INTO Contribution VALUES(96,22,"Songwriter");
+INSERT INTO Contribution VALUES(84,22,"Engineer");
+INSERT INTO Contribution VALUES(97,22,"Producers");
+INSERT INTO Contribution VALUES(98,22,"Recording");
+--Toast
+INSERT INTO Contribution VALUES(99,23,"Engineer");
+INSERT INTO Contribution VALUES(99,23,"Producers");
+INSERT INTO Contribution VALUES(100,23,"Recording");
+--Sacrifice
+INSERT INTO Contribution VALUES(101,24,"Songwriter");
+INSERT INTO Contribution VALUES(102,24,"Engineer");
+INSERT INTO Contribution VALUES(103,24,"Producers");
+INSERT INTO Contribution VALUES(104,24,"Recording");
+--Crazy What Love Can Do
+INSERT INTO Contribution VALUES(105,25,"Feature");
+INSERT INTO Contribution VALUES(106,25,"Feature");
+INSERT INTO Contribution VALUES(107,25,"Songwriter");
+INSERT INTO Contribution VALUES(108,25,"Engineer");
+INSERT INTO Contribution VALUES(109,25,"Producers");
+INSERT INTO Contribution VALUES(110,25,"Recording");
+--Attention
+INSERT INTO Contribution VALUES(111,26,"Feature");
+INSERT INTO Contribution VALUES(112,26,"Songwriter");
+INSERT INTO Contribution VALUES(113,26,"Guitar");
+INSERT INTO Contribution VALUES(113,26,"Percussion/Extra");
+INSERT INTO Contribution VALUES(114,26,"Engineer");
+INSERT INTO Contribution VALUES(115,26,"Producers");
+INSERT INTO Contribution VALUES(116,26,"Recording");
+--Celebrity Skin
+INSERT INTO Contribution VALUES(117,27,"Songwriter");
+INSERT INTO Contribution VALUES(118,27,"Percussion/Extra");
+INSERT INTO Contribution VALUES(119,27,"Engineer");
+INSERT INTO Contribution VALUES(120,27,"Producers");
+INSERT INTO Contribution VALUES(121,27,"Recording");
+--Space Man
+INSERT INTO Contribution VALUES(122,28,"Songwriter");
+INSERT INTO Contribution VALUES(123,28,"Guitar");
+INSERT INTO Contribution VALUES(123,28,"Percussion/Extra");
+INSERT INTO Contribution VALUES(124,28,"Engineer");
+INSERT INTO Contribution VALUES(123,28,"Producers");
+INSERT INTO Contribution VALUES(95,28,"Recording");
+--Flowers
+INSERT INTO Contribution VALUES(125,29,"Songwriter");
+INSERT INTO Contribution VALUES(126,29,"Engineer");
+INSERT INTO Contribution VALUES(125,29,"Producers");
+INSERT INTO Contribution VALUES(127,29,"Recording");
+--Baby
+INSERT INTO Contribution VALUES(128,30,"Songwriter");
+INSERT INTO Contribution VALUES(128,30,"Guitar");
+INSERT INTO Contribution VALUES(129,30,"Percussion/Extra");
+INSERT INTO Contribution VALUES(130,30,"Engineer");
+INSERT INTO Contribution VALUES(128,30,"Producers");
+INSERT INTO Contribution VALUES(131,30,"Recording");
